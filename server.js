967fede974e9ae1d1b625c83843915fa8c232973
Montharob <meemontarob@gmail.com>
2024-02-16 10:56:20 +0700

    books

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+const books = require('./db')
+app.get('/books', (req, res) => {
+  res.json(books)
+})
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
