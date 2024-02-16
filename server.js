03b112b0ba1fd8f6a3117e0b9257ea2d200530cb
Montharob <meemontarob@gmail.com>
2024-02-16 16:13:11 +0700

    Edit-post-book

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.get('/books/:id', (req, res) => {
   res.json(books.find(book => book.id === req.params.id))
 })
 
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(201).json(req.body)
+})
+
 app.put('/books/:id', (req, res) => {
     const updateIndex = books.findIndex(book => book.id === req.params.id)
     res.json(Object.assign(books[updateIndex], req.body))
