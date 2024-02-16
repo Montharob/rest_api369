c1fc21feb7c5f31e6ee7bf38ff2ef0ad74e24e05
Montharob <meemontarob@gmail.com>
2024-02-16 11:10:49 +0700

    book-id

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const books = require('./db')
 app.get('/books', (req, res) => {
   res.json(books)
 })
+app.get('/books/:id', (req, res) => {
+  res.json(books.find(book => book.id === req.params.id))
+})
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
